30ad281e041c9710b72a27db8f68ad6f084dbceb
Developer-Paul-t <phs880623@gmail.com>
2021-12-26 23:05:26 +0900

    DomainProtocol

--- BookFriend/BookFriend/Domain/BookServiceType.swift
+++ BookFriend/BookFriend/Domain/BookServiceType.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol BookServiceType: DomainProtocol where VALUE == Book {
+    var memoryBookDB: [Book] { get set }
+}
--- BookFriend/BookFriend/Domain/DomainProtocol.swift
+++ BookFriend/BookFriend/Domain/DomainProtocol.swift
@@ -11,6 +11,9 @@ import RxSwift
 protocol DomainProtocol {
     associatedtype KEY
     associatedtype VALUE
-    func create()
-    func getAll()
+    func save(_ value: VALUE) -> Observable<Bool>
+    func fetchAll() -> Observable<[VALUE]>
+    func fetch(_ key: KEY) -> Observable<VALUE>
+    func update(_ key: KEY, _ value: VALUE) -> Observable<Bool>
+    func delete(_ key: KEY) -> Observable<Bool>
 }
